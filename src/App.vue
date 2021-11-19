8c6601cb1c1767b08c828e146c9b5deec3bce954
hyemin <hyemin52@naver.com>
2021-11-19 10:44:26 +0900

    Create project

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1>{{message}}</h1>
+    <img src="~assets/logo.png" alt="">
+</template>
+
+<script>
+export default{
+    data() {
+        return {
+            message: 'Hello Vue!'
+        }
+    }
+}
+</script>
